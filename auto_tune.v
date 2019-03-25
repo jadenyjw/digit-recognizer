8e39904c9ae1fcb86ec4b2d0b69bd964a6534f9f
Yiming Zhong <48134703+paulZhongym@users.noreply.github.com>
2019-03-25 12:41:55 -0400

    modify the waveform to produce different sound

--- auto_tune.v
+++ auto_tune.v
@@ -22,12 +22,18 @@ input				reset;
 
 input			[AUDIO_DATA_WIDTH:1]	data_audio_in;
 
-output		[AUDIO_DATA_WIDTH:1]	data_audio_out;
+output reg	[AUDIO_DATA_WIDTH:1]	data_audio_out;
 
 
 
+always @(*)
+begin
+	if(data_audio_in > 32'b1000_0000_0000_0000)
+		data_audio_out = 32'b0101_0101_0101_0101;
+	else
+		assign data_audio_out = data_audio_in;
+end
 
-assign data_audio_out = data_audio_in & 32'b0101_0101_0101_0101;
 
 
 
